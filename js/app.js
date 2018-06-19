10ec9e3015abb8ee9856e4028066597a69c9d0d2
unknown <madjas00@gmail.com>
2018-06-18 21:05:04 -0500

    Images set to change when clicked

--- js/app.js
+++ js/app.js
@@ -1,15 +1,38 @@
 'use strict';
 
+var nextImage = 0;
+//Function for Displaying Images
 function displayImages() {
+    var image1 = Item.all [nextImage++];
+    var img1 = document.getElementById('item-1');
+    img1.src = image1.src;
+
+    var image2 = Item.all [nextImage++];
+    var img2 = document.getElementById('item-2');
+    img2.src = image2.src;
+
+    var image3 = Item.all [nextImage++];
+    var img3 = document.getElementById('item-3');
+    img3.src = image3.src;
+    
 
 }
 
-window.addEventListener('load', displayImages);
+
+var itemImages = document.querySelectorAll('#vote img');
+
+for(var i=0; i < itemImages.length; i++  ) {
+    itemImages[i].addEventListener('click',function(event){
+        console.log('click' ,event.target);
+
+        displayImages();
+    });
+}
 
 //Setting up Object Constructor Function
-function Item (name,image) {
+function Item (name,src) {
     this.name = name;
-    this.image = image;
+    this.src = src;
 
     Item.all.push(this);
 
@@ -27,6 +50,7 @@ new Item('Toilet Tablet', 'img/bathroom.jpg');
 new Item('Banana Peeler', 'img/banana.jpg');
 new Item('Boots', 'img/boots.jpg');
 
+window.addEventListener('load', displayImages);
 console.log(Item.all);
 
 
